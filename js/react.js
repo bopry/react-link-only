bed088dfbb9c0af94adf408ce8aceacaaeaa334c
pryharin_bohdan <pryharin_bohdan@gmail.com>
2025-12-17 12:21:02 +0200

    vsv

--- js/react.js
+++ js/react.js
@@ -1,17 +1,12 @@
-const root = document.getElementById('root');
-
 function App() {
   const [color, setColor] = React.useState('red');
 
   return (
     <h1
-      style={{ color }}
+      style={{ backgroundColor: color }}
       onClick={() => setColor(color === 'red' ? 'blue' : 'red')}
     >
       Привет)
     </h1>
   );
 }
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
